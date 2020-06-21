8aac4bd527519350cca7ce78e93497126d2a3a06
naoto hamada <hamada@vis-its.com>
2020-06-21 14:20:34 +0900

    Add format, read

---
+++ src/read.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.read = (dir) => {
+  let stat;
+
+  try {
+    stat = fs.statSync(dir);
+  } catch (e) {
+    throw new Error(`"${dir}" does not exists.`);
+  }
+
+  if (!stat.isDirectory()) {
+    throw new Error(`"${dir}" can't be opened as a directory.`);
+  }
+
+  const root = {
+    type: 'directory',
+    name: dir,
+    children: readDirectory(dir),
+  };
+
+  return root;
+}
+
+const readDirectory = (dir) => {
+  const dirents = fs.readdirSync(dir, {
+    withFileTypes: true,
+  });
+
+  const nodes = [];
+
+  dirents.forEach((dirent) => {
+    if (dirent.name.startsWith('.')) {
+      return;
+    }
+
+    if (dirent.isFile()) {
+      nodes.push({
+        type: 'file',
+        name: dirent.name,
+      });
+    } else if (dirent.isDirectory()) {
+      nodes.push({
+        type: 'directory',
+        name: dirent.name,
+        children: readDirectory(
+          path.join(dir, dirent.name),
+        ),
+      });
+    }
+  });
+
+  return nodes;
+}
