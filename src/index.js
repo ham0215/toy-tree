cb8ed50f855c14c0a04908b7b7d2493650963c70
naoto hamada <hamada@vis-its.com>
2020-06-21 14:23:22 +0900

    Fix to use read, format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const meow = require('meow');
+const { read } = require('./read');
+const { format } = require('./format');
 
 exports.main = (argv, stdout, stderr) => {
   const cli = meow(
@@ -17,7 +19,18 @@ exports.main = (argv, stdout, stderr) => {
 
   const dir = cli.input[0] || '.';
 
-  stdout(dir);
+  let root;
+
+  try {
+    root = read(dir);
+  } catch (e) {
+    stderr(`Error: ${e.message}`);
+    return 1;
+  }
+
+  const output = format(root);
+
+  stdout(output);
 
   return 0;
 };
